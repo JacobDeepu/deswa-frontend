0dea353c03576f2afb1ab5d4928416673bc11a1c
Jacob Deepu <d2jdeepu@gmail.com>
2023-08-09 08:52:19 +0530

    Fix Navigation menu

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -50,7 +50,9 @@ const Header = () => {
             <header className={"fixed top-0 w-full  z-30 bg-white-500 transition-all " + (scrollActive ? "shadow-md pt-0" : " pt-4")}>
                 <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
                     <div className="col-start-1 col-end-2 flex items-center">
-                        <img src={Logo} alt="DESWA" className="h-8 w-auto" />
+                        <Link to="/">
+                            <img src={Logo} alt="DESWA" className="h-8 w-auto" />
+                        </Link>
                     </div>
                     <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500 items-center">
                         <LinkScroll
@@ -64,43 +66,17 @@ const Header = () => {
                             }}
                             className={
                                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative " +
-                                (activeLink === "home" ? " text-orange-500 animation-active " : "text-black-500 hover:text-orange-500")
+                                (activeLink === "/home" ? " text-orange-500 animation-active " : "text-black-500 hover:text-orange-500")
                             }
                         >
                             Home
                         </LinkScroll>
-                        <LinkScroll
-                            activeClass="active"
-                            to="post-job"
-                            spy={true}
-                            smooth={true}
-                            duration={1000}
-                            onSetActive={() => {
-                                setActiveLink("post-job");
-                            }}
-                            className={
-                                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative " +
-                                (activeLink === "post-job" ? " text-orange-500 animation-active " : "text-black-500 hover:text-orange-500")
-                            }
-                        >
-                            Post Job
-                        </LinkScroll>
-                        <LinkScroll
-                            activeClass="active"
-                            to="post-job"
-                            spy={true}
-                            smooth={true}
-                            duration={1000}
-                            onSetActive={() => {
-                                setActiveLink("post-job");
-                            }}
-                            className={
-                                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative " +
-                                (activeLink === "post-job" ? " text-orange-500 animation-active " : "text-black-500 hover:text-orange-500")
-                            }
-                        >
-                            Job Listing
-                        </LinkScroll>
+                        <Link to="/create-job" className="px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative text-black-500 hover:text-orange-500">
+                            Post a Job
+                        </Link>
+                        <Link to="/list-jobs" className="px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative text-black-500 hover:text-orange-500">
+                            Find a Job
+                        </Link>
                     </ul>
                     <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
                         {isLoggedIn ? (
